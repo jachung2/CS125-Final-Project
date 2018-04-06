6ad1a6ad4e022e42a373f254a10aa290e858ec96
Jonathan Chung <jachung2@illinois.edu>
2018-04-05 19:48:04 -0500

    Test

--- src/main/java/cs125/chatex/MainActivity.java
+++ src/main/java/cs125/chatex/MainActivity.java
@@ -2,6 +2,7 @@ package cs125.chatex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void displayButton(android.view.View view) {
+        TextView textView = (TextView)(findViewById(R.id.textView1));
+        textView.setText("\\\\int");
+
+    }
+    public void clear(android.view.View view) {
+        TextView textView = (TextView)(findViewById(R.id.textView1));
+        textView.setText("");
     }
 }
